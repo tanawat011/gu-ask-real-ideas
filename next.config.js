d424dd11c9c6a5559a0d6d686d5835281b8db3b7
Tanawat Pinthongpan <tanawat@magicboxasia.com>
2022-04-29 20:41:26 +0700

    feat: update script export static file and public path for image

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ const nextConfig = {
     return config
   },
   reactStrictMode: true,
+  images: {
+    loader: 'imgix',
+    path: 'https://tanawat.web.app/',
+    // path: 'http://localhost:3000/',
+  },
 }
 
 module.exports = nextConfig
